e080c7da7a1ac89dd6dcf45037b8d92f2d49e489
Bob T <54815862+appjedi@users.noreply.github.com>
2025-12-15 11:10:04 -0800

    update repo

--- AndroidStudioProjects/VitalsTracker/app/src/main/java/net/timlin/vitalstracker/network/APIService.kt
+++ AndroidStudioProjects/VitalsTracker/app/src/main/java/net/timlin/vitalstracker/network/APIService.kt
@@ -3,10 +3,13 @@ package net.timlin.vitalstracker.network
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
 
     @GET("vitals")
     suspend fun getVitals(): Response<List<VitalsRow>>
+    @POST(value="vitals")
+    suspend fun postVitals(vital: VitalsRow):Response<VitalsRow>
 
 }
--- AndroidStudioProjects/VitalsTracker/app/src/main/java/net/timlin/vitalstracker/network/VitalsRepository.kt
+++ AndroidStudioProjects/VitalsTracker/app/src/main/java/net/timlin/vitalstracker/network/VitalsRepository.kt
@@ -1,11 +1,25 @@
 package net.timlin.vitalstracker.network
 
+import net.timlin.vitalstracker.model.VitalsItem
+
 class VitalsRepository (private val api: ApiService){
-    suspend fun fetchMovies(title:String): Result<List<VitalsRow>> {
+    suspend fun fetchVitals(title:String): Result<List<VitalsRow>> {
         return try {
             val response = api.getVitals()
             if (response.isSuccessful) {
                 Result.success(response.body() ?: emptyList())
+            } else {
+                Result.failure(Exception("Server error"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e) // Network error / no internet
+        }
+    }
+    suspend fun postVitals(vital: VitalsRow): Result<VitalsRow?> {
+        return try {
+            val response = api.postVitals(vital)
+            if (response.isSuccessful) {
+                Result.success(response.body())
             } else {
                 Result.failure(Exception("Server error"))
             }
