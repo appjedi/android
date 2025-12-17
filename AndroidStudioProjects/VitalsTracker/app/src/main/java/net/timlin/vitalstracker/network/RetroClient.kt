daa2607ddaba24ec15fd85f57a47bf7162ece82c
Bob T <54815862+appjedi@users.noreply.github.com>
2025-12-17 13:43:45 -0800

    update repo

--- AndroidStudioProjects/VitalsTracker/app/src/main/java/net/timlin/vitalstracker/network/RetroClient.kt
+++ AndroidStudioProjects/VitalsTracker/app/src/main/java/net/timlin/vitalstracker/network/RetroClient.kt
@@ -9,12 +9,17 @@ object RetrofitClient {
 
     //private val BASE_URL:String = "https://www.omdbapi.com/?apikey=7cfe23df&t="
     //private val BASE_URL:String = "http://10.0.2.2:8080/"
+    private val LOCAL_URL:String = "http://10.0.2.2:8080/"
 
     private val BASE_URL:String = "https://timlin.org/"
     val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
 
+    val locatService = Retrofit.Builder()
+        .baseUrl(LOCAL_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build().create(ApiService::class.java)
     val apiService = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
